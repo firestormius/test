0193e9b6a1814a6f08f659d207df7f49b79f3dbd
green_skin <green_skin@192.168.0.102>
2016-10-09 01:15:23 +0300

    test1

---
+++ TestProject/src/MainClass.java
@@ -0,0 +1,12 @@
+
+public class MainClass {
+
+	public MainClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("main 01");
+	}
+
+}
